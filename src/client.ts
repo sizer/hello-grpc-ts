7bf472e4475910efee29f93bf1750c189db9ad43
sizer <shinse.tanaka@gmail.com>
2019-12-01 22:22:19 +0900

    refactor/ CamelCase

--- src/client.ts
+++ src/client.ts
@@ -1,13 +1,13 @@
 import * as grpc from "grpc";
-import * as book_grpc_pb from "./proto/book_grpc_pb";
-import * as book_pb from "./proto/book_pb";
+import * as bookGrpcPb from "./proto/book_grpc_pb";
+import * as bookPb from "./proto/book_pb";
 
-const client = new book_grpc_pb.BookServiceClient(
+const client = new bookGrpcPb.BookServiceClient(
   "127.0.0.1:50051",
   grpc.credentials.createInsecure()
 );
 
-const req = new book_pb.GetBookRequest();
+const req = new bookPb.GetBookRequest();
 req.setId("book1");
 
 client.getBook(req, function(error, result) {
